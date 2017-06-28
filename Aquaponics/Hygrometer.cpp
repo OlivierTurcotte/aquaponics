cf0ff827acbd20295e48882b6a2bffde75959557
Olivier Turcotte <olivier.turcotte@outlook.com>
2017-06-27 21:55:21 -0400

    Hygrometer now working

--- Aquaponics/Hygrometer.cpp
+++ Aquaponics/Hygrometer.cpp
@@ -15,6 +15,7 @@ void Hygrometer::setup(uint8_t sensor_pin)
 {
 	pinMode(sensor_pin, INPUT);
 	pin = sensor_pin;
+	moisture_top = 450;
 
 }
 
@@ -27,17 +28,18 @@ bool Hygrometer::isMoist() {
 	uint16_t value[10] = {};
 
 	//Value getters
-	for (int i = 0; i < 9; i++) {
+	for (int i = 0; i < 10; i++) {
 		value[i] = analogRead(pin);
 		Serial.println(value[i]);
 		delay(500);
 	}
 	//Average part
 	uint16_t average = 0;
-	for (int i = 0; i < 9; i++) {
+	for (int i = 0; i < 10; i++) {
 		average += value[i];
 	}
 	average = average / 10;
+	Serial.print("AVERAGE:");
 	Serial.println(average);
 	if (average < moisture_top) {
 		return true;
